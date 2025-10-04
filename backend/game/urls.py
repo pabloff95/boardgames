78e5608324f4eaf8d16c256bd16d27b469dfa83e
Pablo <pablo.fernandezfernandez@outlook.com>
2025-10-04 17:49:31 +0200

    Add get by id endpoint

--- backend/game/urls.py
+++ backend/game/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:game_id>", views.game, name="game"),
 ]
--- backend/game/views.py
+++ backend/game/views.py
@@ -1,3 +1,4 @@
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from .models import Game
 
@@ -9,3 +10,12 @@ def index(request):
 
     games_list = list(games)
     return JsonResponse(games_list, safe=False)
+
+
+def game(request, game_id):
+    try:
+        game = Game.objects.get(pk=game_id)
+    except Game.DoesNotExist:
+        return JsonResponse({"error": "Game not found"}, status=404)
+
+    return JsonResponse(model_to_dict(game))
