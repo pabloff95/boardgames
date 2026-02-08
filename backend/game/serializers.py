3b821703713d2d4002a75519d70d1d0fe8173f92
Pablo <pablo.fernandezfernandez@outlook.com>
2026-02-08 11:57:50 +0100

    Pass GameImages as part of the Game response

--- backend/game/serializers.py
+++ backend/game/serializers.py
@@ -8,8 +8,15 @@ class GameRulesSerializer(serializers.ModelSerializer):
         fields = ["id", "game", "version", "content", "created_at", "updated_at"]
 
 
+class GameImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GameImage
+        fields = ["id", "name", "type", "file"]
+
+
 class GameSerializer(serializers.ModelSerializer):
     rules = GameRulesSerializer(many=True, read_only=True)
+    images = GameImageSerializer(many=True, read_only=True)
     saved_by_count = serializers.IntegerField(read_only=True)
     average_score = serializers.FloatField(read_only=True)
 
@@ -26,15 +33,10 @@ class GameSerializer(serializers.ModelSerializer):
             "rules",
             "saved_by_count",
             "average_score",
+            "images",
         ]
 
 
 class GamesOfTheWeekQuerySerializer(serializers.Serializer):
     days_range = serializers.IntegerField(default=7, min_value=1)
     games_limit = serializers.IntegerField(default=5, min_value=1)
-
-
-class GameImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = GameImage
-        fields = ["id", "name", "type", "file"]
