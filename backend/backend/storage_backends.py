1851d8d5c78bfb22f8eb29dbc4e305b362539f4a
Pablo <pablo.fernandezfernandez@outlook.com>
2026-02-15 19:54:05 +0100

    Use cloudfront for file storage

--- backend/backend/storage_backends.py
+++ backend/backend/storage_backends.py
@@ -1,14 +1,18 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+from django.conf import settings
 
 
 class PublicMediaStorage(S3Boto3Storage):
     location = "media"
     default_acl = "public-read"
     file_overwrite = False
+    custom_domain = settings.CLOUDFRONT_DOMAIN
+    secure_urls = True
 
 
 class PrivateMediaStorage(S3Boto3Storage):
     location = "private"
     default_acl = "private"
     file_overwrite = False
-    custom_domain = False
+    custom_domain = settings.CLOUDFRONT_DOMAIN
+    secure_urls = True
